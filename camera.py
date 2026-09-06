86fd8d61f274a04a5a601b264561f845c5e4da0b
Chris-wav <218120177+Chris-wav@users.noreply.github.com>
2026-09-06 23:04:29 +0300

    feat: add frame difference and motion mask

--- camera.py
+++ camera.py
@@ -1,6 +1,7 @@
 import cv2
 
 cap = cv2.VideoCapture(0)
+previous_gray = None
 
 if not cap.isOpened():
     print("Camera didn't open")
@@ -9,13 +10,30 @@ if not cap.isOpened():
 try:
     while True:
         ret, frame = cap.read()
+
         if not ret:
-            print('No picture taken')
+            print("No picture taken")
             break
+
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        cv2.imshow('Grayscale', gray)
+
+        if previous_gray is None:
+            previous_gray = gray
+            continue
+
+        diff = cv2.absdiff(previous_gray, gray)
+
+        _, mask = cv2.threshold(diff, 30, 255, cv2.THRESH_BINARY)
+
+        cv2.imshow("Grayscale", gray)
+        cv2.imshow("Difference", diff)
+        cv2.imshow('Binary Mask', mask)
+
+        previous_gray = gray
+
         if cv2.waitKey(1) & 0xFF == ord("q"):
             break
+
 finally:
     cv2.destroyAllWindows()
     cap.release()
